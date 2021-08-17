9eade797038cb2607c29028ee5520e88c60f55a6
Mark Stott <mypantsareontheoutside@gmail.com>
2021-08-17 14:50:41 +0100

    Initial website configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+# Optimizely 12
+
+Optimizely (Formerly EPiServer) is a CMS platform that has historically been built on the .NET 4.x tech stack.  With the move to Optimizely 12, the technical stack is moving to .NET 5.0.  As part of this, the way that components are built is changing.  This solution is a test bed for my own learning.
---
+++ src/OptimizelyTwelveTest/Features/Home/HomePageController.cs
@@ -0,0 +1,14 @@
+﻿namespace OptimizelyTwelveTest.Features.Home
+{
+    using OptimizelyTwelveTest.Features.Common;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomePageController : PageControllerBase<HomePage>
+    {
+        public IActionResult Index(HomePage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
