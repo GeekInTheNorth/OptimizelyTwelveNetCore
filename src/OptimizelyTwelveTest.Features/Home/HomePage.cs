971ef37e640f0ed05382d2910dc781562a3b6de5
Mark Stott <mypantsareontheoutside@gmail.com>
2021-08-17 15:18:03 +0100

    Move features into a features project.

---
+++ src/OptimizelyTwelveTest.Features/Home/HomePage.cs
@@ -0,0 +1,30 @@
+﻿namespace OptimizelyTwelveTest.Features.Home
+{
+    using System.ComponentModel.DataAnnotations;
+    
+    using EPiServer.Core;
+    using EPiServer.DataAbstraction;
+    using EPiServer.DataAnnotations;
+
+    [ContentType(
+        DisplayName = "Home Page",
+        GUID = "060C7B3A-971D-4632-92C4-B493C2DA8D52",
+        Description = "A page designed as a default landing page.",
+        GroupName = SystemTabNames.Content)]
+    public class HomePage : PageData
+    {
+        [Display(
+            Name = "Heading",
+            Description = "The H1 to display",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [Display(
+            Name = "Main Content Area",
+            Description = "Renders blocks within the main content section of the home page.",
+            GroupName = SystemTabNames.Content, 
+            Order = 20)]
+        public virtual ContentArea MainContentArea { get; set; }
+    }
+}
---
+++ src/OptimizelyTwelveTest.Features/Home/HomePageController.cs
@@ -0,0 +1,14 @@
+﻿namespace OptimizelyTwelveTest.Features.Home
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using OptimizelyTwelveTest.Features.Common;
+
+    public class HomePageController : PageControllerBase<HomePage>
+    {
+        public IActionResult Index(HomePage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
