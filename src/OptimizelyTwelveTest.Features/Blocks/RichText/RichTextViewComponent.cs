fd0fa6a901038b1b8ac19e7315fcff7b6b497b43
Mark Stott <mypantsareontheoutside@gmail.com>
2021-08-17 16:20:24 +0100

    Create a simple block as a ViewComponent

---
+++ src/OptimizelyTwelveTest.Features/Blocks/RichText/RichTextViewComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace OptimizelyTwelveTest.Features.Blocks.RichText
+{
+    using EPiServer.Web.Mvc;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class RichTextBlockViewComponent : BlockComponent<RichTextBlock>
+    {
+        public override IViewComponentResult Invoke(RichTextBlock currentBlock)
+        {
+            return View(currentBlock);
+        }
+    }
+}
