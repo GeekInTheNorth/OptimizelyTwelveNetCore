8c00e9499ff959f83bec867c9b1b5c5dfdd462e0
Mark Stott <mypantsareontheoutside@gmail.com>
2021-08-18 14:13:55 +0100

    Adding custom admin pages.

---
+++ src/OptimizelyTwelveTest.Features/CustomAdmin/CustomAdminPageController.cs
@@ -0,0 +1,28 @@
+﻿namespace OptimizelyTwelveTest.Features.CustomAdmin
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = "CmsAdmin,WebAdmins,Administrators")]
+    [Route("[controller]")]
+    public class CustomAdminPageController : Controller
+    {
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult FunctionOne()
+        {
+            return View("Index");
+        }
+
+        [Route("[action]")]
+        public IActionResult FunctionTwo()
+        {
+            return View("Index");
+        }
+    }
+}
