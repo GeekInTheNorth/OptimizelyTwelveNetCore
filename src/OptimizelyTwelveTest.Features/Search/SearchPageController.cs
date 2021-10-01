8149af34f0510393d1b67b9f5b50bc1384581763
Mark Stott <mypantsareontheoutside@gmail.com>
2021-10-01 12:03:43 +0100

    Set up an initial search page function.

---
+++ src/OptimizelyTwelveTest.Features/Search/SearchPageController.cs
@@ -0,0 +1,25 @@
+﻿namespace OptimizelyTwelveTest.Features.Search
+{
+    using Microsoft.AspNetCore.Mvc;
+    using OptimizelyTwelveTest.Features.Common;
+
+    public class SearchPageController : PageControllerBase<SearchPage>
+    {
+        [HttpGet]
+        public IActionResult Index(SearchPage currentContent)
+        {
+            return View(currentContent);
+        }
+
+        [HttpGet]
+        public IActionResult Search(SearchPage currentContent, string searchText, int page)
+        {
+            return new ContentResult()
+            {
+                Content = "{ \"TODO\": \"Create search content\" }",
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+        }
+    }
+}
